0f5edd1a71e30ead3687156590ebf9be7a1d9d41
meldyssey <meldyssey15@gmail.com>
2025-12-10 16:43:27 +0900

    SSR Intro

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,11 +1,38 @@
 // CSS Module
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
 
-export default function Home() {
+// Next.js에서 약속된 이름으로 내보내주면 SSR로 동작하도록 자동으로 설정됨
+export const getServerSideProps = () => {
+  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
+  // window는 브라우저 뜻해서 서버에서만 동작하는 getServerSideProps에서는 동작하지 못함
+  // window.location;
+  console.log("서버사이드프롭스");
+  const data = "hello";
+
+  return {
+    //리턴값음 객체여야 함 -> 객체를 읽어와서 props를 페이지 컴포넌트에 전달
+    props: {
+      data,
+    },
+  };
+};
+
+//props로 받아오듯이 getServerSideProps의 props를 받아올 수 있음
+export default function Home({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  // 서버에서도 한 번 실행이 되기 때문에 여기서 오류 날 수 있음
+  // window.location;
+  // 마운트 된 이후 실행되게끔 하면 오류 안남
+  useEffect(() => {
+    console.log(window);
+  }, []);
+  console.log(data);
   return (
     <div className={style.container}>
       <section>
