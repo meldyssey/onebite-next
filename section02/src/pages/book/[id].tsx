ae210d98387b6b1173eb83db257f459400d950a6
meldyssey <meldyssey15@gmail.com>
2025-12-10 17:34:21 +0900

    SSR  적용

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -1,4 +1,6 @@
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import style from "./[id].module.css";
+import fetchOneBook from "@/lib/fetch-one-book";
 const moockData = {
   id: 1,
   title: "한 입 크기로 잘라 먹는 리액트",
@@ -11,9 +13,26 @@ const moockData = {
     "https://shopping-phinf.pstatic.net/main_3888828/38888282618.20230913071643.jpg",
 };
 
-export default function Page() {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  // 무조건 존재할 꺼다는 '!' 단언으로
+  const id = context.params!.id;
+  const book = await fetchOneBook(Number(id));
+
+  return {
+    props: {
+      book,
+    },
+  };
+};
+
+export default function Page({
+  book,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  if (!book) return "문제가 발생했습니다. 다시 시도하세요.";
   const { id, title, subTitle, description, author, publisher, coverImgUrl } =
-    moockData;
+    book;
 
   return (
     <div className={style.container}>
