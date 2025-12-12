012fa38b2371859c77f2b00ddb5162e566079b3b
meldyssey <meldyssey15@gmail.com>
2025-12-12 15:54:03 +0900

    SSG 적용

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -2,27 +2,21 @@
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import { ReactNode, useEffect } from "react";
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
-import { InferGetServerSidePropsType } from "next";
+import { InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
 
-// Next.js에서 약속된 이름으로 내보내주면 SSR로 동작하도록 자동으로 설정됨
-export const getServerSideProps = async () => {
-  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
-  // window는 브라우저 뜻해서 서버에서만 동작하는 getServerSideProps에서는 동작하지 못함
-  // window.location;
-  // console.log("서버사이드프롭스");
+// Next.js에서 약속된 이름으로 내보내주면 SSG로 동작하도록 자동으로 설정됨
+export const getStaticProps = async () => {
+  console.log("인덱스 페이지");
 
-  // 병렬로 동작 -> 좀 더 빠르게 렌더링 됨
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
     fetchRandomBooks(),
   ]);
 
   return {
-    //리턴값음 객체여야 함 -> 객체를 읽어와서 props를 페이지 컴포넌트에 전달
     props: {
       allBooks,
       recoBooks,
@@ -30,14 +24,10 @@ export const getServerSideProps = async () => {
   };
 };
 
-//props로 받아오듯이 getServerSideProps의 props를 받아올 수 있음
 export default function Home({
   allBooks,
   recoBooks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  // 서버에서도 한 번 실행이 되기 때문에 여기서 오류 날 수 있음
-  // window.location;
-  // 마운트 된 이후 실행되게끔(useEffect 안에서 실행) 하면 오류 안남
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   useEffect(() => {
     console.log(window);
   }, []);
