6c59e515157a509c1205c867a77ac78700319793
meldyssey <meldyssey15@gmail.com>
2025-12-19 18:55:40 +0900

    SSG 폴백옵션 실습

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -1,6 +1,7 @@
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import style from "./[id].module.css";
 import fetchOneBook from "@/lib/fetch-one-book";
+import { useRouter } from "next/router";
 
 export const getStaticPaths = () => {
   return {
@@ -11,7 +12,10 @@ export const getStaticPaths = () => {
       { params: { id: "3" } },
     ],
     // 존재하지 않는 url에 접속하는 경우 대비책
-    fallback: false,
+    fallback: true,
+    // false: path에 설정하지 않은 경로는 모두 404 Not found
+    // "blocking": SSR 방식으로 실시간으로 페이지 생성(사전렌더링)
+    // true: SSR 방식 + 데이터가 없는 fallback 상태의 페이지(Props 데이터가 없는 상태의 페이지)부터 생성해 반환
   };
 };
 
@@ -19,6 +23,11 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
   // 무조건 존재할 꺼다는 '!' 단언으로
   const id = context.params!.id;
   const book = await fetchOneBook(Number(id));
+  if (!book) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: {
@@ -30,7 +39,12 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
 export default function Page({
   book,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+
+  // fallback 상태일 때 메시지를 띄우는 방법
+  if (router.isFallback) return "로딩중입니다.";
   if (!book) return "문제가 발생했습니다. 다시 시도하세요.";
+
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
 
   return (
