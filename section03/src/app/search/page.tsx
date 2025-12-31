2b529b792de11e92a91811fadc964ef18c962b84
meldyssey <meldyssey15@gmail.com>
2025-12-31 21:17:28 +0900

    App Router 페이지 라우팅 설정

---
+++ section03/src/app/search/page.tsx
@@ -0,0 +1,9 @@
+// async를 붙일 수 있음
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q: string }>;
+}) {
+  const { q } = await searchParams;
+  return <div>Search 페이지 : {q}</div>;
+}
