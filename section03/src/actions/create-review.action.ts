3564e66c3e4bad919e65b42cd29529fc0584fe85
meldyssey <meldyssey15@gmail.com>
2026-01-27 16:57:56 +0900

    서버 액션: 리뷰 추가 기능 구현

---
+++ section03/src/actions/create-review.action.ts
@@ -0,0 +1,26 @@
+// 파일로 나눈 경우 상단으로 위치시키기
+"use server";
+
+export async function createReviewAction(formData: FormData) {
+  const bookId = formData.get("bookId")?.toString();
+  const content = formData.get("content")?.toString();
+  const author = formData.get("author")?.toString();
+
+  if (!bookId || !content || !author) {
+    return;
+  }
+
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
+      {
+        method: "POST",
+        body: JSON.stringify({ bookId, content, author }),
+      },
+    );
+    console.log(response.status);
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+}
