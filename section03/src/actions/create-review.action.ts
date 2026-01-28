894c948ed6727a8771ee0b05afebbe25ec834a0c
meldyssey <meldyssey15@gmail.com>
2026-01-28 20:22:14 +0900

    서버액션: 클라이언트 컴포넌트에서 서버액션 활용

--- section03/src/actions/create-review.action.ts
+++ section03/src/actions/create-review.action.ts
@@ -1,26 +1,34 @@
 // 파일로 나눈 경우 상단으로 위치시키기
 "use server";
 
+import { delay } from "@/util/delay";
 import { revalidatePath, revalidateTag } from "next/cache";
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_: any, formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
 
   if (!bookId || !content || !author) {
-    return;
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요.",
+    };
   }
 
   try {
+    await delay(2000);
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
+      `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/1`,
       {
         method: "POST",
         body: JSON.stringify({ bookId, content, author }),
       },
     );
-    console.log(response.status);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
     /*재검증해서 다시 생성함 
     - 서버측에서만 호출할 수 있는 메서드
     - 해당 페이지의 모든 캐시 무효화 시킴
@@ -48,8 +56,14 @@ export async function createReviewAction(formData: FormData) {
     - 해당 태그 값을 갖는 데이터 캐시가 재검증 됨
      */
     revalidateTag(`review-${bookId}`);
+    return {
+      status: true,
+      error: "",
+    };
   } catch (error) {
-    console.error(error);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다 : ${error}`,
+    };
   }
 }
