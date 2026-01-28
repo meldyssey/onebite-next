ceec9081ae5703eaf41f3148f90a6f988ef49800
meldyssey <meldyssey15@gmail.com>
2026-01-28 20:46:18 +0900

    서버액션: 리뷰 삭제 기능 구현

---
+++ section03/src/components/review-item-delete-button.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { deleteReviewAction } from "@/actions/delete-review.action";
+import React, { useActionState, useEffect, useRef } from "react";
+
+export default function ReviewItemDeleteButton({
+  reviewId,
+  bookId,
+}: {
+  reviewId: number;
+  bookId: number;
+}) {
+  const formRef = useRef<HTMLFormElement>(null);
+  const [state, formAction, isPending] = useActionState(
+    deleteReviewAction,
+    null,
+  );
+
+  useEffect(() => {
+    if (state && !state.status) {
+      alert(state.error);
+    }
+  }, [state]);
+
+  // submit 강제로 제출함
+  // requestSubmit 안전하게 동작할 가능성이 높음
+  return (
+    <form ref={formRef} action={formAction}>
+      <input name="reviewId" value={reviewId} hidden />
+      <input name="bookId" value={bookId} hidden />
+      {isPending ? (
+        <div>...</div>
+      ) : (
+        <div onClick={() => formRef.current?.requestSubmit()}>삭제하기</div>
+      )}
+    </form>
+  );
+}
