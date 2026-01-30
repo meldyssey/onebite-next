fb46adee9a364139ce50e184671c49de9b603b2b
meldyssey <meldyssey15@gmail.com>
2026-01-30 18:32:05 +0900

    최적화: 검색 엔진 최적화

--- section03/src/app/(with-searchbar)/page.tsx
+++ section03/src/app/(with-searchbar)/page.tsx
@@ -5,6 +5,7 @@ import { delay } from "@/util/delay";
 import { Suspense } from "react";
 import BookItemSkeleton from "@/components/skeleton/book-item-skeleton";
 import BookListSkeleton from "@/components/skeleton/book-list-skelton";
+import { Metadata } from "next";
 
 // export const dynamic = "force-dynamic";
 // 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
@@ -17,7 +18,7 @@ async function AllBooks() {
   await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
-    { cache: "force-cache" }
+    { cache: "force-cache" },
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다...</div>;
@@ -37,7 +38,7 @@ async function RecoBooks() {
   await delay(3000);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
-    { next: { revalidate: 3 } }
+    { next: { revalidate: 3 } },
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다...</div>;
@@ -53,6 +54,16 @@ async function RecoBooks() {
 }
 
 export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "한입 북스",
+  description: "한입 북스에 등록된 도서를 만나보세요",
+  openGraph: {
+    title: "한입 북스",
+    description: "한입 북스에 등록된 도서를 만나보세요",
+    images: ["/thumbnail,png"],
+  },
+};
 
 export default function Home() {
   return (
--- section03/src/app/(with-searchbar)/search/page.tsx
+++ section03/src/app/(with-searchbar)/search/page.tsx
@@ -3,6 +3,7 @@ import BookListSkeleton from "@/components/skeleton/book-list-skelton";
 import { BookData } from "@/types";
 import { delay } from "@/util/delay";
 import { Suspense } from "react";
+import { Metadata } from "next";
 
 // export const dynamic = "force-static";
 // query string은 undefined로 됨 -> 동적 함수가 제대로 동작하지 않음
@@ -11,7 +12,7 @@ async function SearchResult({ q }: { q: string }) {
   await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
-    { cache: "force-cache" }
+    { cache: "force-cache" },
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다...</div>;
@@ -26,6 +27,24 @@ async function SearchResult({ q }: { q: string }) {
       ))}
     </div>
   );
+}
+
+export async function generateMetadata({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>;
+}): Promise<Metadata> {
+  // 현재 페이지 메타 데이터를 동적으로 생성하는 역할
+  const { q } = await searchParams;
+  return {
+    title: `${q} : 한입북스 검색`,
+    description: `${q}의 검색 결과입니다.`,
+    openGraph: {
+      title: `${q} : 한입북스 검색`,
+      description: `${q}의 검색 결과입니다.`,
+      images: ["/thumbnail.png"],
+    },
+  };
 }
 
 export default async function Page({
